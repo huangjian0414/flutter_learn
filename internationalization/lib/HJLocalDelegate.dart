1ddfeb7d30f47db9374d906b2893935d681ee7d8
Jayehuang <jianhuangbest@gmail.com>
2021-07-07 13:52:24 +0800

    自己的字符串。国际化

---
+++ internationalization/lib/HJLocalDelegate.dart
@@ -0,0 +1,30 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:internationalization/HJLocalManager.dart';
+
+class HJLocalDelegate extends LocalizationsDelegate<HJLocalManager>{
+  @override
+  ///当前环境的Locale，是否在我们支持的语言范围
+  bool isSupported(Locale locale) {
+    // TODO: implement isSupported
+    return ['en','zh'].contains(locale.languageCode);
+  }
+
+  @override
+  /// 当Locale发生改变时（语言环境），加载对应的HYLocalizations资源
+  Future<HJLocalManager> load(Locale locale) {
+    // TODO: implement load
+    return SynchronousFuture(HJLocalManager(locale));
+  }
+
+  @override
+  ///当Localizations Widget重新build时，是否调用load方法重新加载Locale资源
+  bool shouldReload(covariant LocalizationsDelegate old) {
+    // TODO: implement shouldReload
+    return false;
+  }
+
+  static HJLocalDelegate delegate = HJLocalDelegate();
+}
